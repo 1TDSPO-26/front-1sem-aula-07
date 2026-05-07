f4286965cc6ffcabdd9305bf89767df23efbb803
Matheus <rm573416@fiap.com.br>
2026-05-07 20:40:02 -0300

    feat: Sistema de login com recuperação de dados no evento click e tratamento de erros em try catch.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 
+//Montando um objeto para comparar os dados do formulário
+const usuario = {
+  nome: "Zezinho",
+  email: "zezinho@gmai.com",
+  senha: "LALALA"
+
+}
+
+
 
 //Recuperando botão entrar
 const botaoEntrar = document.getElementById("btnEntrar");
@@ -11,8 +20,34 @@ const botaoEntrar = document.getElementById("btnEntrar");
 
 //Ex: botaoEntrar.addEventListener("click", function(){ Coisas aqui dentro da função para serem executadas quando o botão for clicado. });
 
-botaoEntrar.addEventListener("click", function(){
-  console.log("O botão foi clicado")
-})
+botaoEntrar.addEventListener("click", function(evento){
+  evento.preventDefault();
 
-botaoEntrar();
+try{
+  const email = document.getElementById("idEmail");
+  const senha = document.getElementById("idSenha");
+
+  console.log(email.value);
+  console.log(senha.value);
+
+
+//Vamos comoparar os dados digitados no formulario com os dados do objeto "usuario" e caso seja valido, redirecionar para a pagina index.html.
+
+if(!(emai === null && senha === null)){
+
+  if(email.value === usuario.email && senha.value === usuario.senha){
+    window.location.href = "index.html"
+  }else {
+    throw new Error("Nome de usuário ou senha incorretos!");
+  }
+} else {
+  throw new Error("Preencha todos os campos!")
+
+}
+
+}catch (error){
+  alert(error.message);
+}
+});
+
+
